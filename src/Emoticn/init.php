20f5736466b09c45e7d25ac433d239b4f66382ea
Adam Averay <adam@averay.com>
2014-03-16 17:55:36 -0400

    Fixed path normalisation

--- src/Emoticn/init.php
+++ src/Emoticn/init.php
@@ -3,9 +3,9 @@ require_once(__DIR__.'/../../vendor/autoload.php');
 
 // Load config
 $config	= json_decode(file_get_contents(__DIR__.'/config.json'), true);
-array_walk($config['paths'], function($path){
+array_walk($config['paths'], function(&$path){
 	// Make relative paths absolute
-	return (($path === '' || $path[0] !== '/') ? __DIR__.'/' : '').$path;
+	$path	= (($path === '' || $path[0] !== '/') ? __DIR__.'/' : '').$path;
 });
 
 $env	= new Emoticn\Environment($config);
